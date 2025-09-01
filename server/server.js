78a8269f83195d7a3ef5a3bbf8a7ffb159b642b5
dengus14 <ddgusev0318@gmail.com>
2025-09-01 17:23:45 -0500

    database setup

---
+++ server/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const connectDB = require('./config/db');
+const express = require('express')
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 4001
+
+const router = express.Router()
+//connecting to the database, in db.js file
+connectDB()
+
+//cors and http parser
+app.use(cors({
+  origin: "https://localhost:3000",  
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}
+));
+app.use(express.json());
+
+
+//basic routing
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/health', (req, res) => {
+  console.log('Health check hit');
+  res.json({ status: 'ok' });
+});
+
+app.listen(port, () => {
+  console.log(`ChatBot listening at ${port}`)
+})
+
